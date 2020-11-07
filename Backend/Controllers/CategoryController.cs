73d2496e9c6c5927d5097ca5b945f60d12f1c079
MacKarp <email@email.com>
2020-11-07 11:29:54 +0100

    Added API Controllers

---
+++ Backend/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Backend.Data;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IBackendRepo _repository;
+
+        public CategoryController(IBackendRepo repository)
+        {
+            _repository = repository;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> GetAllCategories()
+        {
+            var item = _repository.GetAllCategories();
+            return Ok(item);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Category> GetCategoryById(int id)
+        {
+            var item = _repository.GetCategoryById(id);
+            if (item != null)
+            {
+                return Ok(item);
+            }
+            else return NotFound();
+        }
+    }
+}
---
+++ Backend/Controllers/ItemController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Backend.Data;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemController : ControllerBase
+    {
+        private readonly IBackendRepo _repository;
+
+        public ItemController(IBackendRepo repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Item>> GetAllItems()
+        {
+            var item = _repository.GetAllItems();
+            return Ok(item);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Expire> GetItemById(int id)
+        {
+            var item = _repository.GetItemById(id);
+            if (item != null)
+            {
+                return Ok(item);
+            }
+            else return NotFound();
+        }
+    }
+}
