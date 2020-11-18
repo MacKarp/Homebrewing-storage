158241569bf0376da2b77b36524cacce41bcce05
MacKarp <email@email.com>
2020-11-18 13:57:50 +0100

    Added comments to ReadDto

--- Backend/Dtos/ExpireReadDto.cs
+++ Backend/Dtos/ExpireReadDto.cs
@@ -6,8 +6,8 @@ namespace Backend.Dtos
     {
         public int ExpireId { get; set; }
         public int UserId { get; set; } //temporary, need to change to proper UserID
-        public virtual Storage IdStorage { get; set; }
-        public virtual Item IdItem { get; set; }
+        public virtual Storage IdStorage { get; set; } //ustawić mapowanie na int zamiast zwracać cały obiekt 
+        public virtual Item IdItem { get; set; } //ustawić mapowanie na int zamiast zwracać cały obiekt 
         public string ExpirationDate { get; set; } //need to change to proper Data format
     }
 }
