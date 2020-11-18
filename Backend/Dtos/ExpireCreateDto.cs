8eb15037174572c0d12d458e44c92adcbd83f5a5
MacKarp <email@email.com>
2020-11-18 14:57:31 +0100

    Add Create DTO

---
+++ Backend/Dtos/ExpireCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Models;
+
+namespace Backend.Dtos
+{
+    public class ExpireCreateDto
+    {
+        public int UserId { get; set; } //temporary, need to change to proper UserID
+        public virtual Storage IdStorage { get; set; } //ustawić mapowanie na int zamiast zwracać cały obiekt 
+        public virtual Item IdItem { get; set; } //ustawić mapowanie na int zamiast zwracać cały obiekt 
+        public string ExpirationDate { get; set; } //need to change to proper Data format
+    }
+}
---
+++ Backend/Dtos/StorageCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Dtos
+{
+    public class StorageCreateDto
+    {
+        public int UserID { get; set; } //temporary, need to change to proper UserID
+        public string StorageName { get; set; }
+    }
+}
