d9dc731be8ee9b576e4c89f5c26d77a5afe411d4
LK-Herman <lkuczma@gmail.com>
2020-11-25 18:08:24 +0100

    Added UserProfile for AutoMapper

---
+++ Backend/Profiles/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Backend.Dtos;
+using Backend.Models;
+
+namespace Backend.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserReadDto>();
+            CreateMap<UserCreateDto, User>();
+            CreateMap<UserUpdateDto, User>();
+            CreateMap<User, UserUpdateDto>();
+        }
+    }
+}
