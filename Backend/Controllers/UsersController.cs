94b4e47754d0f0cf5d9607cd9efb8752c5f19a3a
Karpfly <karpfly@gmail.com>
2020-12-04 14:11:56 +0100

    Added ActionResult to UserController
    added:
    - GetAllUsers
    - GetUserById
    - CreateUser
    - UpdateUser
    - PartialUpdateUser
    - DeleteUser

--- Backend/Controllers/UsersController.cs
+++ Backend/Controllers/UsersController.cs
@@ -1,10 +1,10 @@
 ﻿using AutoMapper;
 using Backend.Data;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Backend.Dtos;
+using Backend.Models;
+using Microsoft.AspNetCore.JsonPatch;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,6 +23,92 @@ namespace Backend.Controllers
             _mapper = mapper;
         }
 
-       
+        [HttpGet]
+        public ActionResult<IEnumerable<UserReadDto>> GetAllUsers()
+        {
+            var item = _repository.GetAllUsers();
+            if (item != null)
+            {
+                return Ok(_mapper.Map<IEnumerable<UserReadDto>>(item));
+            }
+            else return NotFound();
+        }
+
+        [HttpGet("{id}", Name = "GetUserById")]
+        public ActionResult<UserReadDto> GetUserById(int id)
+        {
+            var item = _repository.GetUserById(id);
+            if (item != null)
+            {
+                return Ok(_mapper.Map<UserReadDto>(item));
+            }
+            else return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<UserReadDto> CreateUser(UserCreateDto userCreateDto)
+        {
+            var model = _mapper.Map<User>(userCreateDto);
+            _repository.CreateUser(model);
+            _repository.SaveChanges();
+
+            var readDto = _mapper.Map<UserReadDto>(model);
+
+            return CreatedAtRoute(nameof(GetUserById), new { Id = readDto.UserId }, readDto);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateUser(int id, UserUpdateDto userUpdateDto)
+        {
+            var modelFromRepo = _repository.GetUserById(id);
+            if (modelFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(userUpdateDto, modelFromRepo);
+            _repository.UpdateUser(modelFromRepo);
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public ActionResult PartialUpdateUser(int id, JsonPatchDocument<UserUpdateDto> patchDocument)
+        {
+            var modelFromRepo = _repository.GetUserById(id);
+            if (modelFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            var toPatch = _mapper.Map<UserUpdateDto>(modelFromRepo);
+            patchDocument.ApplyTo(toPatch, ModelState);
+            if (!TryValidateModel(toPatch))
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            _mapper.Map(toPatch, modelFromRepo);
+            _repository.UpdateUser(modelFromRepo);
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteUser(int id)
+        {
+            var modelFromRepo = _repository.GetUserById(id);
+            if (modelFromRepo == null)
+            {
+                return NotFound();
+            }
+            _repository.DeleteUser(modelFromRepo);
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+
     }
 }
