6a647e10652af99c4996562c9cbeff60a38be508
Karpfly <karpfly@gmail.com>
2020-12-07 15:03:09 +0100

    Updated Expire, Item, Storage profiles map

--- Backend/Profiles/ExpireProfile.cs
+++ Backend/Profiles/ExpireProfile.cs
@@ -9,10 +9,18 @@ namespace Backend.Profiles
     {
         public ExpireProfile()
         {
-            CreateMap<Expire, ExpireReadDto>().ForMember(dest => dest.IdUser, opt => opt.MapFrom(src => src.IdUser.UserId)).ForMember(dst => dst.IdStorage, opt => opt.MapFrom(src => src.IdStorage.StorageId)).ForMember(dst => dst.IdItem, opt => opt.MapFrom(src => src.IdItem.ItemId));
-            CreateMap<ExpireCreateDto, Expire>();
-            CreateMap<ExpireUpdateDto, Expire>();
-            CreateMap<Expire, ExpireUpdateDto>();
+            CreateMap<Expire, ExpireReadDto>()
+                .ForMember(dest => dest.IdUser, opt => opt.MapFrom(src => src.IdUser.UserId))
+                .ForMember(dst => dst.IdStorage, opt => opt.MapFrom(src => src.IdStorage.StorageId))
+                .ForMember(dst => dst.IdItem, opt => opt.MapFrom(src => src.IdItem.ItemId));
+            CreateMap<ExpireUpdateDto, Expire>()
+                 .ForPath(dest => dest.IdUser.UserId, opt => opt.MapFrom(src => src.UserId))
+                 .ForPath(dst => dst.IdStorage.StorageId, opt => opt.MapFrom(src => src.IdStorage))
+                 .ForPath(dest => dest.IdItem.ItemId, opt => opt.MapFrom(src => src.IdItem));
+            CreateMap<Expire, ExpireUpdateDto>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.IdUser.UserId))
+                .ForMember(dst => dst.IdStorage, opt => opt.MapFrom(src => src.IdStorage.StorageId))
+                .ForMember(dst => dst.IdItem, opt => opt.MapFrom(src => src.IdItem.ItemId));
         }
     }
 }
